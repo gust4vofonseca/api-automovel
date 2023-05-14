a035560f343ab27001ef3998ce7fcdb247a41f74
Gustavo Fonseca <gustavofonseca27@gmail.com>
2023-05-14 11:54:21 -0300

    Feat: alteracao

--- src/config/__test__/setup-db.ts
+++ src/config/__test__/setup-db.ts
@@ -4,19 +4,7 @@ require('ts-node/register');
 require('tsconfig-paths/register');
 
 export default async () => {
-  const startTime = new Date().getTime();
-
   await dataSource.initialize();
 
-  const connectTime = new Date().getTime();
-
   await dataSource.runMigrations();
-
-  const migrationsTime = new Date().getTime();
-
-  console.log(
-    `\nConnected in ${connectTime - startTime}ms - Executed migrations in ${
-      migrationsTime - connectTime
-    }ms.\n`,
-  );
 };
